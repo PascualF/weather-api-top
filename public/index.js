c0e1e23922c5d1d804d53b0b746cfddb514d9729
Pascual Felicio <pascualfelicio@gmail.com>
2025-03-20 13:41:50 +0100

    Final, add some extras personal stuff

--- public/index.js
+++ public/index.js
@@ -1,8 +1,11 @@
 const divCelsius = document.querySelector(".temp-celsius")
+const divF = document.querySelector(".temp-fahr")
 const conditionParagraph = document.querySelector(".condition-current")
 const btnConfirm = document.querySelector("button")
 const inputUser = document.querySelector("input")
 const iconWeather = document.querySelector(".weather-icon")
+const titleCity = document.querySelector(".city-title")
+const cityDefault = 'Brussels'
 
 const getApiKey = async () => {
     try{
@@ -15,18 +18,19 @@ const getApiKey = async () => {
 }
 
 const iconsObject = {
-    'Clear': './icons/clear-sky.png',
+    'Clear': './icons/sun.png',
     'Rain, Partially cloudy': './icons/rain.png',
     'Overcast': './icons/dark.png',
     'Partially cloudy': './icons/cloudy.png'
 }
 
-const getWeather = async (city = 'brussels') => {
+const getWeather = async (city = cityDefault) => {
     try{
         const apiKey = await getApiKey()
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`)
         const getInfo = await response.json()
         const getCurrentDay = getInfo.currentConditions
+        titleCity.textContent = city
         return {
             currentCondition : getCurrentDay.conditions,
             currentTemp : getCurrentDay.temp
@@ -36,23 +40,31 @@ const getWeather = async (city = 'brussels') => {
     }
 }
 
-const displayWeather = async () => {
+const displayWeather = async (userInput = cityDefault) => {
     try{
-        console.log('working')
-        const currentWeather = await getWeather()
-        console.log(currentWeather)
+        const currentWeather = await getWeather(userInput)
         conditionParagraph.textContent = currentWeather.currentCondition
-        console.log(iconsObject)
-        console.log(iconsObject['Overcast'])
+        iconWeather.src = iconsObject[currentWeather.currentCondition]
+        divCelsius.textContent = `${Math.round(convertFToC(currentWeather.currentTemp))} °C`
+        divF.textContent =  `${Math.round(currentWeather.currentTemp)} °F`
     } catch(err){
         console.log(`Error: ${err}`)
     }
 }
 
-getWeather()
+displayWeather()
 
 btnConfirm.addEventListener('click', () => {
-    displayWeather()
+    
+    // Get User Input
+    const valueUser = inputUser.value
+
+    console.log(valueUser)
+    if(valueUser === ''){
+        displayWeather()
+    } else {
+        displayWeather(valueUser)
+    }
 })
 
 
